20bb090753859b2eb08d24df2ea7aa761c5e201c
Luiz Eduardo <luizeof@gmail.com>
2018-05-19 14:51:02 -0300

    Incluindo o LearnDash

---
+++ lesson.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+<main id="main" class="site-main" role="main">
+
+	<div class="page-content">
+		<?php the_content(); ?>
+	</div>
+
+</main>
+
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
